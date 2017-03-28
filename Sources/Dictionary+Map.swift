2590afd735f5a2bed70ca62a52e63da4cbe3d90d
Pavel Sharanda <edvaef@gmail.com>
2017-03-29 00:30:59 +0300

    encoder inplace coding

--- Sources/Dictionary+Map.swift
+++ Sources/Dictionary+Map.swift
@@ -15,3 +15,16 @@ extension Dictionary {
         return result
     }
 }
+
+extension Dictionary {
+    
+    mutating func merge(with dictionary: Dictionary) {
+        dictionary.forEach { updateValue($1, forKey: $0) }
+    }
+    
+    func merged(with dictionary: Dictionary) -> Dictionary {
+        var dict = self
+        dict.merge(with: dictionary)
+        return dict
+    }
+}
--- Sources/Encoder.swift
+++ Sources/Encoder.swift
@@ -18,6 +18,7 @@ public final class Encoder {
         return _dictionary
     }
     
+    //MARK: value
     public func encode<T: Encodable>(_ value: T, key: String) {
         _dictionary[key] = Encoder.encode(value)
     }
@@ -26,6 +27,7 @@ public final class Encoder {
         _dictionary[key] = Encoder.encode(value, skipIfNil: skipIfNil)
     }
     
+    //MARK: array
     public func encode<T: Encodable>(_ value: [T], key: String) {
         _dictionary[key] = Encoder.encode(value)
     }
@@ -34,6 +36,7 @@ public final class Encoder {
         _dictionary[key] = Encoder.encode(value, skipIfNil: skipIfNil)
     }
     
+    //MARK: dict
     public func encode<T: Encodable>(_ value: [String: T], key: String) {
         _dictionary[key] = Encoder.encode(value)
     }
@@ -42,6 +45,7 @@ public final class Encoder {
         _dictionary[key] = Encoder.encode(value, skipIfNil: skipIfNil)
     }
     
+    //MARK: dict of arrays
     public func encode<T: Encodable>(_ value: [String: [T]], key: String) {
         _dictionary[key] = Encoder.encode(value)
     }
@@ -50,6 +54,17 @@ public final class Encoder {
         _dictionary[key] = Encoder.encode(value, skipIfNil: skipIfNil)
     }
     
+    //MARK: set
+    public func encode<T: Encodable & Hashable>(_ value: Set<T>, key: String) {
+        _dictionary[key] = Encoder.encode(value)
+    }
+    
+    public func encode<T: Encodable & Hashable>(_ value: Set<T>?, key: String, skipIfNil: Bool = false) {
+        _dictionary[key] = Encoder.encode(value, skipIfNil: skipIfNil)
+    }
+    
+    //MARK: static
+    //MARK: value
     public static func encode<T: Encodable>(_ value: T) -> Any {
         return value.encode()
     }
@@ -58,6 +73,7 @@ public final class Encoder {
         return value.flatMap { $0.encode() } ?? (skipIfNil ? nil : NSNull())
     }
     
+    //MARK: array
     public static func encode<T: Encodable>(_ value: [T]) -> Any {
         return value.map { $0.encode() }
     }
@@ -66,6 +82,7 @@ public final class Encoder {
         return value.flatMap { $0.map { $0.encode() } } ?? (skipIfNil ? nil : NSNull())
     }
     
+    //MARK: dict
     public static func encode<T: Encodable>(_ value: [String: T]) -> Any {
         return value.map { $0.encode() }
     }
@@ -74,11 +91,31 @@ public final class Encoder {
         return value.flatMap { $0.map { $0.encode() } } ?? (skipIfNil ? nil : NSNull())
     }
     
+    //MARK: dict of arrays
     public static func encode<T: Encodable>(_ value: [String: [T]]) -> Any {
         return value.map { $0.map { $0.encode() } }
     }
     
     public static func encode<T: Encodable>(_ value: [String: [T]]?, skipIfNil: Bool = false) -> Any? {
         return value.flatMap { $0.map { $0.map { $0.encode() } } } ?? (skipIfNil ? nil : NSNull())
+    }
+    
+    //MARK: set
+    public static func encode<T: Encodable & Hashable>(_ value: Set<T>) -> Any {
+        return value.map { $0.encode() }
+    }
+    
+    public static func encode<T: Encodable>(_ value: Set<T>?, skipIfNil: Bool = false) -> Any? {
+        return value.flatMap { $0.map { $0.encode() } } ?? (skipIfNil ? nil : NSNull())
+    }
+    
+    //inplace
+    
+    public func encode(encoder: Encoder) {
+        _dictionary.merge(with: encoder.dictionary)
+    }
+    
+    public func encode(encoder: Encoder, key: String) {
+        _dictionary["key"] = encoder.dictionary
     }
 }
